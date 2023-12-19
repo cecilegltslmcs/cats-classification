4ed255582d04360b653df39276e60f154d0a2f7e
Cecile Guillot <cecile@guillot-salmacis.com>
2023-12-19 16:38:13 +0100

    feat: prepare backend & frontend for deployment

--- api/backend.py
+++ api/backend.py
@@ -1,6 +1,65 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, File, UploadFile
+import grpc
+import tensorflow as tf
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2_grpc
+from keras_image_helper import create_preprocessor
+import os
+from proto import np_to_protobuf
+
+host = os.getenv("TF_SERVING_HOST", "localhost:8500")
+channel = grpc.insecure_channel(host)
+stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+preprocessor = create_preprocessor("resnet50", target_size=(224, 224))
 
 app = FastAPI()
+
+
+def prepare_request(X):
+    pb_request = predict_pb2.PredictRequest()
+    pb_request.model_spec.name = "cats-classifier"
+    pb_request.model_spec.signature_name = "serving_default"
+
+    pb_request.inputs["input_6"].CopyFrom(np_to_protobuf(X))
+
+    return pb_request
+
+
+classes = [
+    "Ragdoll",
+    "Bombay",
+    "American Curl",
+    "Birman",
+    "Egyptian Mau",
+    "Bengal",
+    "Siamese",
+    "Maine Coon",
+    "American Bobtail",
+    "Turkish Angora",
+    "Russian Blue",
+    "British Shorthair",
+    "Persian",
+    "Abyssinian",
+    "Norwegian Forest",
+    "Manx",
+    "Scottish Fold",
+    "American Shorthair",
+    "Exotic Shorthair",
+    "Sphynx",
+]
+
+
+def prepare_response(pb_response):
+    preds = pb_response.outputs["dense_5"].float_val
+    return dict(zip(classes, preds))
+
+
+def predict(path):
+    X = preprocessor.from_path(path)
+    pb_request = prepare_request(X)
+    pb_response = stub.Predict(pb_request, timeout=20.0)
+    response = prepare_response(pb_response)
+    return response
 
 
 @app.get("/")
@@ -9,5 +68,13 @@ def home():
 
 
 @app.post("/predict")
-def predict():
-    pass
+async def predict_endpoint(request: Request, file: UploadFile = File(...)):
+    result = predict(file.file)
+    return result
+
+
+if __name__ == "__main__":
+    # url = 'https://image.petmd.com/files/styles/978x550/public/2023-07/scottish-fold.jpg?w=1080&q=75'
+    # response = predict(url)
+    # print(response)
+    app.run(debug=True, host="0.0.0.0")
--- api/frontend.py
+++ api/frontend.py
@@ -1,5 +1,8 @@
 # import libraries
 import streamlit as st
+import pandas as pd
+import requests
+import plotly.express as px
 
 # Displaying in head
 st.title("Cat Breeds Classifier")
@@ -8,3 +11,19 @@ st.text("Upload a cat picture to obtain its breed. Only 20 breeds available.")
 
 # Displaying uploader
 uploaded_file = st.file_uploader("Upload your image...", type="jpg")
+
+
+if uploaded_file is not None:
+    st.image(uploaded_file.getvalue())
+    file = {"file": uploaded_file.getvalue()}
+    res = requests.post("http://localhost:8000/predict", files=file)
+    res_path = res.json()
+    sorted_answer = sorted(res_path.items(), key=lambda x: x[1], reverse=True)
+    converted_dict = dict(sorted_answer, index=[0])
+    answer_df = pd.DataFrame.from_dict(converted_dict, orient="index")
+    answer_df.reset_index(inplace=True)
+    answer_df.rename(columns={0: "Percentage", "index": "Breed"}, inplace=True)
+    answer_df["Percentage"] = answer_df["Percentage"] * 100
+    st.write("Purr! I'm : ")
+    fig = px.pie(answer_df.head(3), names="Breed", values="Percentage")
+    st.plotly_chart(fig, use_container_width=True)
