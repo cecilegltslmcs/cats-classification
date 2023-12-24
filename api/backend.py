007d87441bb5c136132599e949c7e3455b083767
Cecile Guillot <cecile@guillot-salmacis.com>
2023-12-25 00:21:46 +0100

    fix: refacto

--- api/backend.py
+++ api/backend.py
@@ -6,26 +6,22 @@ from tensorflow_serving.apis import prediction_service_pb2_grpc
 from keras_image_helper import create_preprocessor
 from proto import np_to_protobuf
 import uvicorn
+import logging
 
-host = os.getenv("TF_SERVING_HOST", "localhost:8500")
-channel = grpc.insecure_channel(host)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+TF_SERVING_HOST = os.getenv("TF_SERVING_HOST", "localhost:8500")
+HOST = "0.0.0.0"
+PORT = 8000
+
+channel = grpc.insecure_channel(TF_SERVING_HOST)
 stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+
 preprocessor = create_preprocessor("resnet50", target_size=(224, 224))
 
-app = FastAPI()
-
-
-def prepare_request(X):
-    pb_request = predict_pb2.PredictRequest()
-    pb_request.model_spec.name = "cats-classifier"
-    pb_request.model_spec.signature_name = "serving_default"
-
-    pb_request.inputs["input_2"].CopyFrom(np_to_protobuf(X))
-
-    return pb_request
-
-
-classes = [
+CLASSES = [
     "Ragdoll",
     "Bombay",
     "American Curl",
@@ -48,18 +44,34 @@ classes = [
     "Sphynx",
 ]
 
+app = FastAPI()
+
+
+def prepare_request(X):
+    pb_request = predict_pb2.PredictRequest()
+    pb_request.model_spec.name = "cats-classifier"
+    pb_request.model_spec.signature_name = "serving_default"
+
+    pb_request.inputs["input_2"].CopyFrom(np_to_protobuf(X))
+
+    return pb_request
+
 
 def prepare_response(pb_response):
     preds = pb_response.outputs["dense_1"].float_val
-    return dict(zip(classes, preds))
+    return dict(zip(CLASSES, preds))
 
 
 def predict(path):
-    X = preprocessor.from_path(path)
-    pb_request = prepare_request(X)
-    pb_response = stub.Predict(pb_request, timeout=20.0)
-    response = prepare_response(pb_response)
-    return response
+    try:
+        X = preprocessor.from_path(path)
+        pb_request = prepare_request(X)
+        pb_response = stub.Predict(pb_request, timeout=20.0)
+        response = prepare_response(pb_response)
+        return response
+    except Exception as e:
+        logger.error(f"Prediction failed: {e}")
+        return {"error": "Prediction failed"}
 
 
 @app.post("/predict")
@@ -69,4 +81,4 @@ async def predict_endpoint(request: Request, file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host=HOST, port=PORT)
