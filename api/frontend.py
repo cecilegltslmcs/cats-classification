1304f159cdb15582e41e3d9fe15846f57444341b
Cecile Guillot <cecile@guillot-salmacis.com>
2023-12-25 00:58:42 +0100

    fix: refacto

--- api/frontend.py
+++ api/frontend.py
@@ -1,36 +1,64 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 # import libraries
-import streamlit as st
-import pandas as pd
-import requests
-import plotly.express as px
 import os
-
-host = os.getenv("BACKEND_URL", "http://localhost:8000/predict")
-
-# Displaying in head
-st.title("Cats Breeds Classifier")
-st.image("https://cdn.pixabay.com/photo/2023/12/08/23/46/cat-8438334_960_720.jpg")
-st.text("Upload a cat picture to obtain its breed. Only 20 breeds available.")
-
-# Displaying uploader
-uploaded_file = st.file_uploader("Upload your image...", type="jpg")
+import pandas as pd
+import plotly.express as px
+import requests
+import streamlit as st
 
 
-if uploaded_file is not None:
-    st.image(uploaded_file.getvalue())
-    file = {"file": uploaded_file.getvalue()}
-    res = requests.post(f"http://{host}/predict", timeout=20, files=file)
-    res_path = res.json()
-    sorted_answer = sorted(res_path.items(), key=lambda x: x[1], reverse=True)
-    converted_dict = dict(sorted_answer, index=[0])
-    answer_df = pd.DataFrame.from_dict(converted_dict, orient="index")
-    answer_df.reset_index(inplace=True)
-    answer_df.rename(columns={0: "Percentage", "index": "Breed"}, inplace=True)
-    answer_df.drop(index=answer_df.index[-1], axis=0, inplace=True)
-    answer_df["Percentage"] = answer_df["Percentage"] * 100
-    st.subheader("🐱 Purr! I'm a :")
-    fig = px.pie(answer_df.head(3), names="Breed", labels="Breed", values="Percentage")
-    fig.update_traces(hoverinfo="label+percent", textinfo="label+value")
+def get_predictions(image_file):
+    backend_url = os.getenv("BACKEND_URL", "http://localhost:8000/predict")
+    file = {"file": image_file}
 
-    st.plotly_chart(fig, use_container_width=True)
-    st.dataframe(answer_df)
+    try:
+        response = requests.post(
+            f"http://{backend_url}/predict", timeout=20, files=file
+        )
+        response.raise_for_status()
+
+        predictions = response.json()
+        sorted_predictions = sorted(
+            predictions.items(), key=lambda x: x[1], reverse=True
+        )
+        converted_dict = dict(sorted_predictions, index=[0])
+        answer_df = pd.DataFrame.from_dict(converted_dict, orient="index")
+        answer_df.reset_index(inplace=True)
+        answer_df.rename(columns={0: "Percentage", "index": "Breed"}, inplace=True)
+        answer_df.drop(index=answer_df.index[-1], axis=0, inplace=True)
+        answer_df["Percentage"] = answer_df["Percentage"] * 100
+        return answer_df
+
+    except requests.RequestException as e:
+        st.error(f"Error: {str(e)}")
+        return None
+
+
+def main():
+    st.title("Cats Breeds Classifier")
+    st.image("https://cdn.pixabay.com/photo/2023/12/08/23/46/cat-8438334_960_720.jpg")
+    st.text("Upload a cat picture to obtain its breed. Only 20 breeds available.")
+
+    uploaded_file = st.file_uploader("Upload your image...", type="jpg")
+
+    if uploaded_file is not None:
+        st.image(uploaded_file.getvalue())
+
+        # Get predictions
+        predictions = get_predictions(uploaded_file.getvalue())
+
+        if predictions is not None:
+            st.subheader("🐱 Purr! I'm a :")
+            fig = px.pie(
+                predictions.head(3), names="Breed", labels="Breed", values="Percentage"
+            )
+            fig.update_traces(hoverinfo="label+percent", textinfo="label+value")
+
+            st.plotly_chart(fig, use_container_width=True)
+            st.dataframe(predictions)
+
+
+if __name__ == "__main__":
+    main()
