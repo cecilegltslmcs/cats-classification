d72424af9b67544028f115d524e7056902151181
Cecile Guillot <cecile@guillot-salmacis.com>
2023-12-24 23:16:45 +0100

    fix: correction

--- api/frontend.py
+++ api/frontend.py
@@ -8,7 +8,7 @@ import os
 host = os.getenv("BACKEND_URL", "http://localhost:8000/predict")
 
 # Displaying in head
-st.title("Cat Breeds Classifier")
+st.title("Cats Breeds Classifier")
 st.image("https://cdn.pixabay.com/photo/2023/12/08/23/46/cat-8438334_960_720.jpg")
 st.text("Upload a cat picture to obtain its breed. Only 20 breeds available.")
 
