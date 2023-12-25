ac44e6489497b27ad964f7e951fc8acaef14d070
Cécile Guillot <cecile@guillot-salmacis.com>
2023-12-25 11:09:08 +0000

    fix: add shebang

--- tests/test_proto.py
+++ tests/test_proto.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
 import unittest
 import numpy as np
 from tensorflow.core.framework import tensor_pb2, tensor_shape_pb2, types_pb2
@@ -16,19 +18,15 @@ class TestTensorConversion(unittest.TestCase):
         data = np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)
         tensor_proto = make_tensor_proto(data)
 
-        # Add your assertions based on the expected values
         self.assertEqual(tensor_proto.dtype, types_pb2.DT_FLOAT)
         self.assertEqual(len(tensor_proto.tensor_shape.dim), 2)
-        # Add more assertions as needed
 
     def test_np_to_protobuf(self):
         data = np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)
         tensor_proto = np_to_protobuf(data)
 
-        # Add your assertions based on the expected values
         self.assertEqual(tensor_proto.dtype, types_pb2.DT_FLOAT)
         self.assertEqual(len(tensor_proto.tensor_shape.dim), 2)
-        # Add more assertions as needed
 
 
 if __name__ == "__main__":
